f8f68530cccf166cd879e2ccc6aed6811dd0910d
Dightmerc <dightmerc@gmail.com>
2019-06-05 11:22:54 +0500

    Blog, Post Models changed | Added BlogList template

--- common/blog/models.py
+++ common/blog/models.py
@@ -5,6 +5,20 @@ from django.utils import timezone
 
 # Create your models here.
 
+
+
+
+class Blog(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
@@ -13,19 +27,12 @@ class Post(models.Model):
     
     published_date = models.DateTimeField(blank=True, null=True)
 
+    blog = models.ForeignKey(Blog, default="", on_delete=models.CASCADE)
+
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-
-    def __str__(self):
-        return self.title
-
-
-class Blog(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-    title = models.CharField(max_length=200)
-    description = models.TextField(max_length=500)
 
     def __str__(self):
         return self.title
